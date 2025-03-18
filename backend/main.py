75b3f03f8c85962005b221d1bd42e887a252cba9
Cameron Beneteau <cameron.beneteau@gmail.com>
2025-03-18 09:31:14 -0400

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:3000"
+    
+]}})
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Test api endpoint"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
---
+++ frontend/src/pages/data.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+
+interface APIData {
+  message: string;
+}
+
+interface APIDataPageProps {
+  data: APIData;
+}
+
+export const getServerSideProps: GetServerSideProps<APIDataPageProps> = async () => {
+  try {
+    const res = await fetch("http://0.0.0.0:5555/api/data");
+    if (!res.ok) throw new Error("Failed to fetch API data");
+
+    const data: APIData = await res.json();
+    return { props: { data } };
+  } catch (error) {
+    return { props: { data: { message: "Error fetching data" } } };
+  }
+};
+
+export default function APIDataPage({ data }: APIDataPageProps) {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-2xl font-bold mb-4">API Data Page</h1>
+      <div className="border p-4 rounded shadow-lg bg-gray-100 dark:bg-gray-800">
+        <h2 className="text-lg font-semibold">Message from API:</h2>
+        <p className="text-md">{data.message || "No message available"}</p>
+      </div>
+    </div>
+  );
+}
